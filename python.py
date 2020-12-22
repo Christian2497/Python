96321b9c608d8886c0fd20d38c1d8958d25101fa
Christian <christian_8_neus@hotmail.com>
2020-12-22 09:38:23 +0100

    Ejercicio 7 hecho

--- python.py
+++ python.py
@@ -28,5 +28,17 @@ print((numero + "\n") * int(numero))
 nombre = input("¿Cual es tu nombre?")
 print(nombre.upper() + " tiene " + str(len(nombre)) + " letras")
 
+# Escribir un programa que realice la siguiente operación aritmética 
+
+print(((3+2)/(2*5)) **2)
+
+# Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla 
+# la paga que le corresponde.
+
+horas = float(input("Introduce tus horas de trabajo: "))
+coste = float(input("Introduce lo que cobras por hora: "))
+paga = round(horas * coste, 3)
+print("Tu paga es " + str(paga))
+
 
 
